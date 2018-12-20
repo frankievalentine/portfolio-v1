1f49e35bc207d7d46552851609928f74bb11b360
frankievalentine <frankievalentine9@gmail.com>
2018-12-19 17:58:31 -0800

    removed contentful plugin and other packages not used

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,6 @@ module.exports = {
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: 'vq4hg572zgbu',
-        accessToken: 'd5ad19eba399477704d1b3c567b84b8a221d25cd7aa28d700c2ea447cfd98851',
-      },
-    },
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-styled-components`,
   ],
